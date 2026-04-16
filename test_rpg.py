e1b254e17bcffc15b34df4c37cf75af05d3973cb
Fernando Ossiel Cruz Medina <ossielfer.cruz@gmail.com>
2026-04-16 16:01:57 -0600

    test: prueba de dano al personaje (RED)

--- test_rpg.py
+++ test_rpg.py
@@ -8,3 +8,13 @@ def test_personaje_nace_con_estadisticas_correctas():
     assert heroe.hp == 1000
     assert heroe.nivel == 1
     assert heroe.esta_vivo == True
+
+def test_personaje_recibe_dano():
+    heroe = Personaje()
+    enemigo = Personaje()
+
+    # Act (Actuar)
+    enemigo.atacar(heroe, dano=200)
+
+    # Assert
+    assert heroe.hp == 800
